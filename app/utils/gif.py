ef07fdb50b87076217414dfbcc24f73e72a60ac0
iver56 <iver56@hotmail.com>
2019-02-10 15:11:47 +0100

    Tweak gif frame durations

--- app/utils/gif.py
+++ app/utils/gif.py
@@ -5,11 +5,16 @@ import imageio
 from app.utils.files import get_file_paths
 
 
-def make_gif(folder, use_every_n=9):
+def make_gif(folder, use_every_n=20, include_last_frame=True):
     frame_paths = get_file_paths(folder)
-    frame_paths = [path for i, path in enumerate(frame_paths) if i % use_every_n == 0]
+    frame_paths = [
+        path
+        for i, path in enumerate(frame_paths)
+        if (include_last_frame and i == len(frame_paths) - 1) or i % use_every_n == 0
+    ]
     gif_output_path = os.path.join(folder, "evolution.gif")
-    with imageio.get_writer(gif_output_path, mode="I") as writer:
-        for frame_path in frame_paths:
-            image = imageio.imread(frame_path)
-            writer.append_data(image)
+
+    durations = [0.166] * len(frame_paths)
+    durations[-1] = 1.0
+    images = [imageio.imread(frame_path) for frame_path in frame_paths]
+    imageio.mimsave(gif_output_path, images, duration=durations)
