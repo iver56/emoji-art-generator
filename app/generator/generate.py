b2d76b1eb3f381fcfabd8297447213aa7ef44a90
iver56 <iver56@hotmail.com>
2019-02-10 14:14:36 +0100

    Always make gif at end of evolution

--- app/generator/generate.py
+++ app/generator/generate.py
@@ -119,3 +119,5 @@ if __name__ == "__main__":
             new_population.append(individual)
 
         population = new_population
+
+    make_gif(OUTPUT_DIR / experiment_id)
--- app/generator/make_gif.py
+++ app/generator/make_gif.py
@@ -4,6 +4,8 @@ from app.utils.gif import make_gif
 
 
 if __name__ == "__main__":
+    """Make gif from frames in the most recent experiment folder. This can be useful if you
+    stopped the experiment before it reached the gif-making phase at the end."""
     experiment_folders = get_subfolders(OUTPUT_DIR)
     experiment_folders.sort(key=lambda f: f.name)
     most_recent_experiment_folder = experiment_folders[-1]
