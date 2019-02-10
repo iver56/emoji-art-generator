706b764778b5025f2d30ffb830ceaa641de77eae
iver56 <iver56@hotmail.com>
2019-02-10 15:26:42 +0100

    Use linspace for sampling frames for gif

--- app/utils/gif.py
+++ app/utils/gif.py
@@ -1,17 +1,25 @@
 import os
 
 import imageio
+import numpy as np
 
 from app.utils.files import get_file_paths
 
 
-def make_gif(folder, use_every_n=20, include_last_frame=True):
+def make_gif(folder, max_num_frames=16):
     frame_paths = get_file_paths(folder)
-    frame_paths = [
-        path
-        for i, path in enumerate(frame_paths)
-        if (include_last_frame and i == len(frame_paths) - 1) or i % use_every_n == 0
-    ]
+
+    if len(frame_paths) > max_num_frames:
+        frame_indexes = np.linspace(
+            start=0,
+            stop=len(frame_paths) - 1,
+            endpoint=True,
+            num=max_num_frames,
+            dtype=np.int,
+        ).tolist()
+        frame_indexes = set(frame_indexes)
+        frame_paths = [path for i, path in enumerate(frame_paths) if i in frame_indexes]
+
     gif_output_path = os.path.join(folder, "evolution.gif")
 
     durations = [0.166] * len(frame_paths)
